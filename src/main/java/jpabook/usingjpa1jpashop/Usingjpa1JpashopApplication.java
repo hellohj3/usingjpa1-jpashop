2b2a7737658fa8c52217223b479dca18f9fe8fdb
Myhyem <hellohj_@naver.com>
2021-02-07 12:26:22 +0900

    create project

---
+++ src/main/java/jpabook/usingjpa1jpashop/Usingjpa1JpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.usingjpa1jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usingjpa1JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Usingjpa1JpashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpabook/usingjpa1jpashop/Usingjpa1JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.usingjpa1jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Usingjpa1JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
